6b2459967e8a7e0309aeeae8e4965a76bcc9ac18
javoPB <pbjavouam@gmail.com>
2020-11-02 10:29:41 -0600

    Se modifica pom.xml y clases del proyecto

--- src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.microservices.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfigurationController {
@@ -16,4 +17,21 @@ public class LimitsConfigurationController {
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+	public LimitConfiguration retrieveConfiguration() {
+		//Se genera un exception al proposito para ver como funciona la tolerancia a fallas, es este caso se manda llamar el metodo "fallbackRetrieveConfiguration()"
+		throw new RuntimeException("Not available");
+	}
+	
+	/**
+	 * Este es el método que se invocaría en caso de que al consumir el servicio "/fault-tolerance-example" éste fallara 
+	 * @return
+	 *    Una configuración por default con los valores 9, 999
+	 */
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(999, 9);
+	}
+	
 }
--- src/main/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplication.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/LimitsServiceApplication.java
@@ -2,12 +2,15 @@ package com.in28minutes.microservices.limitsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableHystrix
 public class LimitsServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LimitsServiceApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(LimitsServiceApplication.class, args);
 	}
 
 }
